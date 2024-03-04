c805dfd8b26995b771d1e24fb3515f0a442dd04c
Loan311 <ouarraglolo@gmail.com>
2024-03-04 09:06:16 +0100

    maj 04/03/2024

--- database-connection.php
+++ database-connection.php
@@ -1,4 +1,19 @@
 <?php
+?php
+// on paramètre mysqli pour afficher toutes les erreurs, et s'arrêter lorsqu'il y en a une
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+/* on déclare une variable $connection à la BDD de test (on crée un objet de connexion)
+ le mot-clé global indique que cette variable sera partagée avec d’autres fichiers */
+global $connection;
+/* mysqli_connect() retourne un objet qui représente la connexion
+ au serveur MySQL, ou false si une erreur survient.
+*/
+$connection = mysqli_connect("localhost", "root", null, "pokemontp", 3306);
+if (!$connection) {
+ // en cas d'erreur, la fonction mysqli_connect_error() indique la cause de l'échec de la connexion
+ throw new RuntimeException("Cannot connect to the database. Cause : " . mysqli_connect_error());
+}
+
 /*
 Ce fichier permet de définir un objet de connexion $databaseConnection 
 que vous pouvez utiliser dans chaque page qui nécessite de faire une requête au SGBD
@@ -9,9 +24,10 @@ global $databaseConnection;
 
 /* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
 $databaseConnection = mysqli_connect(
-    'URL de la machine hébergeant votre base de données',
-    "nom de l'utilisateur",
-    "mot de passe de l'utilisateur",
-    "nom de la base de données à laquelle se connecter",
-    "numéro de port ouvert"
+    'http://localhost/phpmyadmin/index.php?route=/database/sql&db=pokemontp',
+    "root",
+    "null",
+    "pokemontp",
 );
+
+
